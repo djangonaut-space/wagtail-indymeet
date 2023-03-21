0ca4a40a33b14cd612b261f0328c8125fafdfc9c
dawnwages <dawn.wages@gmail.com>
2023-03-21 17:29:01 -0400

    add pylint requirement, move dotenv requirement to dev

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from .forms import CustomUserCreationForm
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
