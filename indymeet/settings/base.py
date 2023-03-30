83c44409a718f48cb5893fcc327c45c3410bffc0
dawnwages <dawn.wages@gmail.com>
2023-03-30 11:49:19 -0400

    postgres attributes for dev container

--- indymeet/settings/base.py
+++ indymeet/settings/base.py
@@ -103,9 +103,9 @@ WSGI_APPLICATION = "indymeet.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'local_djangonaut_space',                      # Or path to database file if using sqlite3.
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 4,                  # Not used with sqlite3.
+        'PASSWORD': 'postgres',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': 5432,                      # Set to empty string for default. Not used with sqlite3.
     },
