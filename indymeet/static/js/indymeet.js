fca39a5be502ae5416558a313ee323c65b00eb7d
Tim Schilling <schillingt@better-simple.com>
2023-10-05 06:51:27 -0500

    Clean up the base and landing templates for the particles JS.

--- indymeet/static/js/indymeet.js
+++
@@ -1,5 +0,0 @@
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particules-js', 'static/js/particles/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
---
+++ indymeet/static/js/load_particles.js
@@ -0,0 +1,7 @@
+
+// Requires <script type="text/javascript" src="/static/js/particles/particles.min.js"></script>
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particules-js', '/static/js/particles/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
