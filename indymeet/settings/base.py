f78af063bb3b6cced9082f2f6661ab9416e1f0ab
dawnwages <dawn.wages@gmail.com>
2023-12-05 22:02:25 -0500

    Adding Azure BLOB storage for media and static with CDN endpoint

--- indymeet/settings/base.py
+++ indymeet/settings/base.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # azure storage
+    "storages",
 ]
 
 MIDDLEWARE = [
---
+++ indymeet/settings/storages.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from storages.backends.azure_storage import AzureStorage
+
+
+class AzureMediaStorage(AzureStorage):
+    account_name = settings.AZURE_ACCOUNT_NAME
+    account_key = settings.AZURE_STORAGE_KEY
+    azure_container = settings.AZURE_MEDIA_CONTAINER
+    expiration_secs = None
+    overwrite_files = True
+
+
+class AzureStaticStorage(AzureStorage):
+    account_name = settings.AZURE_ACCOUNT_NAME
+    account_key = settings.AZURE_STORAGE_KEY
+    azure_container = settings.AZURE_STATIC_CONTAINER
+    expiration_secs = None
+
+
+"""
+# If we want to switch to S3 for Media storage
+
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
+
+class PrivateMediaStorage(S3Boto3Storage):
+    location = 'private'
+    default_acl = 'private'
+    file_overwrite = False
+    custom_domain = False
+"""
