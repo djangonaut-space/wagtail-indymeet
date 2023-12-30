56565d7d18ab88435c2e52099593f57d62e40a80
Sarah Abderemane <sarahabderemane@gmail.com>
2023-12-30 06:37:19 +0100

    Update static directories

--- indymeet/settings/base.py
+++ indymeet/settings/base.py
@@ -167,6 +167,8 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static"),
+    os.path.join(BASE_DIR, "theme", "static"),
+    os.path.join(BASE_DIR, "theme", "static_src"),
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
