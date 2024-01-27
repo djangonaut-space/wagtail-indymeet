fa278282b6870730a55790544a68692c47486f0b
dawnwages <dawn.wages@gmail.com>
2024-01-27 14:27:01 -0500

    fix media issue

--- indymeet/settings/storages.py
+++ indymeet/settings/storages.py
@@ -6,7 +6,7 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
     account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
+    account_key = settings.AZURE_STORAGE_KEY
     azure_container = settings.AZURE_MEDIA_CONTAINER
     expiration_secs = None
     overwrite_files = True
@@ -14,7 +14,7 @@ class AzureMediaStorage(AzureStorage):
 
 class AzureStaticStorage(AzureStorage):
     account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
+    account_key = settings.AZURE_STORAGE_KEY
     azure_container = settings.AZURE_STATIC_CONTAINER
     expiration_secs = None
     overwrite_files = True
