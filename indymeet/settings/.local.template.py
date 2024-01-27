b621c9dc68ad133f26fb01f2e7c1ba01e3d84ce3
dawnwages <dawn.wages@gmail.com>
2024-01-27 15:56:35 -0500

    add template for NPM BIN PATH for tailwindcss support

---
+++ indymeet/settings/.local.template.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------
+# For local issues
+# ----------------------------------------------
+import os
+
+print("----------------------------------")
+print("----------------------------------")
+print("LOCAL")
+print("----------------------------------")
+print("----------------------------------")
+
+# uncomment for tailwind NPM executable in your system
+# NPM_BIN_PATH = os.environ.get("NPM_BIN_PATH", "False")
