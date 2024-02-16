d52b946646ae412dc9560f9a9efcc1465ac81fcd
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-02-16 12:38:17 +0100

    Add tests for profile and activate.

--- accounts/tests/test_unsubscribe_view.py
+++ accounts/tests/test_unsubscribe_view.py
@@ -10,21 +10,29 @@ class UnsubscribeViewTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = CustomUser.objects.create_user(username='test', email='example@example.com', password='')
+        cls.user = CustomUser.objects.create_user(
+            username="test", email="example@example.com", password=""
+        )
         cls.user.refresh_from_db()
         cls.user.profile.receiving_newsletter = True
         cls.user.profile.receiving_program_updates = True
         cls.user.profile.receiving_event_updates = True
         cls.user.profile.save()
-        cls.unsubscribe_url = reverse("unsubscribe", kwargs={"user_id": cls.user.id, "token": "dummytoken"})
+        cls.unsubscribe_url = reverse(
+            "unsubscribe", kwargs={"user_id": cls.user.id, "token": "dummytoken"}
+        )
 
     def test_user_does_not_exist(self):
-        response = self.client.get(reverse("unsubscribe", kwargs={"user_id": 500, "token": "dummytoken"}))
+        response = self.client.get(
+            reverse("unsubscribe", kwargs={"user_id": 500, "token": "dummytoken"})
+        )
         self.assertEqual(response.status_code, 404)
 
     def test_redirect_when_unauthenticated(self):
         response = self.client.get(self.unsubscribe_url)
-        self.assertRedirects(response, f"{reverse('login')}?next={self.unsubscribe_url}")
+        self.assertRedirects(
+            response, f"{reverse('login')}?next={self.unsubscribe_url}"
+        )
 
     def test_unsubscribe(self):
         self.client.force_login(self.user)
