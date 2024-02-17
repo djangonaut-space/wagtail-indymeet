8e19e10fd9324a6b87f7cc330aa5ee2e764a06f1
tschilling <schillingt@better-simple.com>
2024-02-17 08:55:33 -0600

    Switch to using DATABASE_URL for db connections.
    This reduces the number of environment variables we need for database
    connections. This will make environment configuration easier.
    
    Closes #281

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,3 +1,8 @@
 python manage.py tailwind install
 python manage.py tailwind build
 python manage.py collectstatic --noinput
+if [ -z "$DATABASE_URL" ]; then
+    echo "Missing DATABASE_URL, migrations will not be run."
+elif
+    python manage.py migrate
+fi
