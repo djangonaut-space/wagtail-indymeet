e6b147c510175f9c03a000fd84310a92b878e9f2
tschilling <schillingt@better-simple.com>
2024-02-17 10:06:39 -0600

    Revise supervisor install to install on server
    Oryx's post build command won't install the dependencies
    within the image. The next place I know we can install them is
    from within the startup command.

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,8 +1,3 @@
 python manage.py tailwind install
 python manage.py tailwind build
 python manage.py collectstatic --noinput
-# Install supervisor
-apt-get update -y
-apt-get install -y supervisor
-# Test what happens when we try to create a sample file
-touch test.txt
