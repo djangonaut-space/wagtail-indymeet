ca24ff337bf12cce529f5d264970bf63c8b76a19
tschilling <schillingt@better-simple.com>
2024-02-17 09:46:17 -0600

    I want to know if this file ends up in the app service or not.

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -4,3 +4,5 @@ python manage.py collectstatic --noinput
 # Install supervisor
 apt-get update -y
 apt-get install -y supervisor
+# Test what happens when we try to create a sample file
+touch test.txt
