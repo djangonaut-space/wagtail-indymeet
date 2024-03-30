c69cb2e88763a665dc1874bf5bb0c18a6ba73a7d
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-03-30 20:56:51 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import PasswordResetView¶
+from django.contrib.auth.views import PasswordResetView
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.shortcuts import render
