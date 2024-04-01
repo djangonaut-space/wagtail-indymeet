fc2dccdf24321485096d6e43585ec8895ab8a845
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-04-01 14:34:47 +0200

    Add a user survey response view. (#370)

--- home/forms.py
+++ home/forms.py
@@ -31,7 +31,7 @@ def make_choices(question: Question) -> list[tuple[str, str]]:
 
 
 class BaseSurveyForm(forms.Form):
-    def __init__(self, survey, user, *args, **kwargs):
+    def __init__(self, *args, survey, user, **kwargs):
         self.survey = survey
         self.user = user if user.is_authenticated else None
         self.field_names = []
@@ -158,3 +158,22 @@ class CreateUserSurveyResponseForm(BaseSurveyForm):
                 value=value,
                 user_survey_response=user_survey_response,
             )
+
+
+class UserSurveyResponseForm(BaseSurveyForm):
+    def __init__(self, *args, instance, **kwargs):
+        self.survey = instance.survey
+        self.user_survey_response = instance
+        super().__init__(*args, survey=self.survey, user=instance.user, *args, **kwargs)
+        self._set_initial_data()
+
+    def _set_initial_data(self):
+        question_responses = self.user_survey_response.userquestionresponse_set.all()
+
+        for question_response in question_responses:
+            field_name = f"field_survey_{question_response.question.id}"
+            if question_response.question.type_field == TypeField.MULTI_SELECT:
+                self.fields[field_name].initial = question_response.value.split(",")
+            else:
+                self.fields[field_name].initial = question_response.value
+            self.fields[field_name].disabled = True
--- home/tests/test_user_survey_response_form_views.py
+++ home/tests/test_user_survey_response_form_views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from accounts.factories import UserFactory
 from home.factories import QuestionFactory
 from home.factories import SurveyFactory
+from home.factories import UserQuestionResponseFactory
 from home.factories import UserSurveyResponseFactory
 from home.models import UserQuestionResponse
 from home.models import UserSurveyResponse
@@ -72,3 +73,52 @@ class CreateUserSurveyResponseFormViewTests(TestCase):
             ).value,
             "Amazing",
         )
+
+
+class UserSurveyResponseViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.survey = SurveyFactory.create(
+            name="Test Survey", description="This is a description of the survey!"
+        )
+        cls.user = UserFactory.create()
+        cls.question_1 = QuestionFactory.create(
+            survey=cls.survey,
+            label="How are you?",
+        )
+        cls.question_2 = QuestionFactory.create(
+            survey=cls.survey,
+            label="What is your favourite food?",
+        )
+        cls.survey_response = UserSurveyResponseFactory(
+            survey=cls.survey, user=cls.user
+        )
+        UserQuestionResponseFactory(
+            user_survey_response=cls.survey_response,
+            question=cls.question_1,
+            value="Very good",
+        )
+        UserQuestionResponseFactory(
+            user_survey_response=cls.survey_response,
+            question=cls.question_2,
+            value="Pizza",
+        )
+        cls.url = reverse("user_survey_response", kwargs={"pk": cls.survey_response.id})
+
+    def test_success_get(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Survey")
+        self.assertContains(response, "This is a description of the survey!")
+        self.assertContains(response, "How are you?")
+        self.assertContains(response, "Very good")
+        self.assertContains(response, "What is your favourite food?")
+        self.assertContains(response, "Pizza")
+        self.assertNotContains(response, "Submit")
+
+    def test_cannot_view_others_survey_response(self):
+        different_user = UserFactory.create()
+        self.client.force_login(different_user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
