8de389c87cdb1009de84374fa9caa7d900b09aa4
Tim Schilling <schillingt@better-simple.com>
2025-12-09 17:07:05 -0600

    Remove organizer info from sessions card.

--- home/views/sessions.py
+++ home/views/sessions.py
@@ -35,15 +35,6 @@ class SessionListView(ListView):
         return (
             Session.objects.with_applications(user=self.request.user)
             .select_related("application_survey")
-            .prefetch_related(
-                Prefetch(
-                    "session_memberships",
-                    queryset=SessionMembership.objects.organizers().select_related(
-                        "user"
-                    ),
-                    to_attr="prefetched_organizers",
-                )
-            )
             .order_by("-end_date")
         )
 
