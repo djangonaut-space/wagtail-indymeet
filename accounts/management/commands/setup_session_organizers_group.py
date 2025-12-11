80e5bf0e6ea27c92ce047668e3c3fba57a525a8e
Tim Schilling <schillingt@better-simple.com>
2025-12-11 10:14:15 -0600

    Update the session organizers group to be created on release.

--- accounts/management/commands/setup_session_organizers_group.py
+++ accounts/management/commands/setup_session_organizers_group.py
@@ -5,6 +5,8 @@ Management command to create or update the Session Organizers group with require
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
+from django.db import transaction
+from django.db.models import Q
 
 from accounts.models import CustomUser, UserAvailability, UserProfile
 from home.models import (
@@ -24,9 +26,6 @@ class Command(BaseCommand):
     help = "Create or update the Session Organizers group with required permissions."
 
     def handle(self, *args, **options) -> None:
-        group, created = Group.objects.get_or_create(name="Session Organizers")
-        group.permissions.clear()
-
         permissions_to_add = [
             (CustomUser, ["view"]),
             (ResourceLink, ["view", "add"]),
@@ -42,27 +41,48 @@ class Command(BaseCommand):
             (SessionMembership, ["view", "add", "change"]),
         ]
 
-        permission_count = 0
-        for model, perm_types in permissions_to_add:
-            content_type = ContentType.objects.get_for_model(model)
-            for perm_type in perm_types:
-                permission = Permission.objects.get(
-                    content_type=content_type,
-                    codename=f"{perm_type}_{model._meta.model_name}",
-                )
-                group.permissions.add(permission)
-                permission_count += 1
+        with transaction.atomic():
+            group, _ = Group.objects.get_or_create(name="Session Organizers")
 
-        team_ct = ContentType.objects.get_for_model(Team)
-        form_team_perm = Permission.objects.get(
-            content_type=team_ct, codename="form_team"
-        )
-        group.permissions.add(form_team_perm)
-        permission_count += 1
-
-        action = "Created" if created else "Updated"
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"{action} Session Organizers group with {permission_count} permissions."
+            # Get all content types in a single query
+            models = [model for model, _ in permissions_to_add]
+            content_types = ContentType.objects.get_for_models(
+                *models, for_concrete_models=False
             )
-        )
+
+            # Build list of (content_type_id, codename) tuples for all permissions
+            permission_lookups = [(content_types[Team].id, "form_team")]
+            for model, perm_types in permissions_to_add:
+                content_type = content_types[model]
+                for perm_type in perm_types:
+                    permission_lookups.append(
+                        (content_type.id, f"{perm_type}_{model._meta.model_name}")
+                    )
+
+            permission_query = Q()
+            for ct_id, codename in permission_lookups:
+                permission_query |= Q(content_type_id=ct_id, codename=codename)
+
+            permissions = Permission.objects.filter(permission_query)
+            desired_permission_ids = set(permissions.values_list("id", flat=True))
+
+            # Add the permissions
+            GroupPermission = Group.permissions.through
+            GroupPermission.objects.bulk_create(
+                [
+                    GroupPermission(group_id=group.id, permission_id=perm_id)
+                    for perm_id in desired_permission_ids
+                ],
+                ignore_conflicts=True,
+            )
+            # Remove any extra permissions that shouldn't be there
+            GroupPermission.objects.filter(group=group).exclude(
+                permission_id__in=desired_permission_ids
+            ).delete()
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Set up Session Organizers group with {len(desired_permission_ids)} "
+                    "permissions."
+                )
+            )
