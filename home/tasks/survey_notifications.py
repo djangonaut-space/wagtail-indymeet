9111f838270c75d369c9453ebea41a39b7d2e0d3
Tim Schilling <schillingt@better-simple.com>
2026-01-17 18:29:31 -0600

    Move the application email logic to the background task processor
    This removes another bottleneck for handling increased numbers of applicants.

---
+++ home/tasks/survey_notifications.py
@@ -0,0 +1,29 @@
+from accounts.models import UserAvailability
+from home import email
+from home.models import UserSurveyResponse, Session
+
+
+def _send_application_notification(email_template: str, survey_response: UserSurveyResponse, session: Session):
+    """Send an email to the user about the application."""
+    availability, _ = UserAvailability.objects.get_or_create(user=survey_response.user)
+    context = {
+        "user": survey_response.user,
+        "name": survey_response.user.first_name or survey_response.user.email,
+        "availability": availability,
+        "response": survey_response,
+        "session": session,
+        "cta_link": survey_response.get_full_url(),
+    }
+    email.send(
+        email_template=email_template,
+        recipient_list=[survey_response.user.email],
+        context=context,
+    )
+
+def send_application_created_notification(survey_response: UserSurveyResponse, session: Session):
+    """Send an email to the user about the created application."""
+    _send_application_notification("application_created", survey_response, session)
+
+def send_application_updated_notification(survey_response: UserSurveyResponse, session: Session):
+    """Send an email to the user about the updated application."""
+    _send_application_notification("application_updated", survey_response, session)
