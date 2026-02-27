5f7e16cf1180c07964fccec37b52717d3d4b41d7
Raffaella <45825990+raffaellasuardini@users.noreply.github.com>
2026-02-27 17:21:30 +0100

    feat: added development email preview endpoint

---
+++ home/views/dev_email_preview.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from datetime import timedelta
+from django.utils import timezone
+
+
+def preview_acceptance_reminder(request):
+    class DummyMembership:
+        acceptance_deadline = timezone.now() + timedelta(days=3)
+
+    class DummySession:
+        start_date = timezone.now()
+        end_date = timezone.now() + timedelta(days=30)
+
+    context = {
+        "membership": DummyMembership(),
+        "session": DummySession(),
+    }
+
+    return render(request, "email/acceptance_reminder/body.html", context)
+
+
+def preview_session_accepted(request):
+    class DummyMembership:
+        acceptance_deadline = timezone.now() + timedelta(days=3)
+
+    class DummySession:
+        start_date = timezone.now()
+        end_date = timezone.now() + timedelta(days=30)
+
+    context = {
+        "membership": DummyMembership(),
+        "session": DummySession(),
+    }
+
+    return render(request, "email/session_accepted/body.html", context)
