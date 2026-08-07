3c0f94d7dd620f40728a66e53670a4910c748d3f
Tim Schilling <schillingt@better-simple.com>
2026-08-07 15:26:34 -0500

    Remove Wagtai's accessibility checker for now.
    It was rendering content that impacted our application leading to
    wasted time debugging. Obviously the solution is to fix the accessiblity
    problems, but that's an unknown amount of time.

--- home/apps.py
+++ home/apps.py
@@ -1,9 +1,25 @@
 from django.apps import AppConfig
 from wagtail.images.apps import WagtailImagesAppConfig
+from wagtail import hooks
 
 
 class HomeAppConfig(AppConfig):
     name = "home"
+
+    def ready(self):
+        from wagtail.admin.userbar import AccessibilityItem
+
+        @hooks.register("construct_wagtail_userbar")
+        def remove_frontend_accessibility_checks(request, items, page=None):
+            """
+            The accessibility checker renders some content that caused things to appear
+            wonky and lead to wasted time debugging. Removing for now.
+            """
+            items[:] = [
+                item
+                for item in items
+                if not (isinstance(item, AccessibilityItem) and not item.in_editor)
+            ]
 
 
 class CustomImagesAppConfig(WagtailImagesAppConfig):
